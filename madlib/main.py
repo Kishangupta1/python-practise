86361fb226cbb105e914700bdd64899735942e04
kishan <kishangupta3469@gmail.com>
2022-08-07 18:56:58 +0530

    completed with computer guessing

--- madlib/main.py
+++ madlib/main.py
@@ -13,4 +13,24 @@ def guess_number(num):
     print(f"you guessed:{guess} and correct no. {random_num}")
 
 
-guess_number(10)
+def computer_guess(num):
+    low = 1
+    high = num
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        # print(guess)
+        feedback = input(f"Is {guess}, l or h or c\n").lower()
+        if feedback == 'l':
+            low = guess + 1
+        elif feedback == 'h':
+            high = guess - 1
+
+    print(f"comp guess correct: {guess}")
+
+
+# guess_number(10)
+computer_guess(20)
